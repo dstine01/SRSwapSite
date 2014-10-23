ca53438c4aee8361187e39622d968e6d63ab6937
DuaneS <dstine01@baker.edu>
2014-10-22 21:28:52 -0500

    Contact Form
    Java Enabled Contact Form Entry

---
+++ js/welcome.js
@@ -0,0 +1,7 @@
+window.confirm ("Welcome to my SR swap resource page");
+var r = confirm("All Content Found on this page are for reference purpose only: I do not hold responsibility for any damage or injury due to the information provided on this page... To Continue Click OK");
+if (r == true) {
+    x = "You pressed OK!";
+} else {
+    x = "You pressed Cancel!";
+}
